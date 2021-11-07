efe6afcee2ad4f684e6aa5c9c5cfe34745ea52cd
jamilrayhan <rjamil336@gmail.com>
2021-11-08 01:10:45 +0600

    remov local date

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ async function run() {
     // GET API
     app.get("/appointments", verifyToken, async (req, res) => {
       const email = req.query.email;
-      const date = new Date(req.query.date).toLocaleDateString();
+      const date = req.query.date;
       const query = { email: email, date: date };
       // console.log(query);
       const cursor = appointmentsCollection.find(query);
